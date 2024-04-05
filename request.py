0f7d287510dd92695084f419e3bc2d4c04872e2b
Jane :3 <132292704+stupidjohanna@users.noreply.github.com>
2024-04-06 01:14:18 +0200

    Update request.py

--- request.py
+++ request.py
@@ -8,5 +8,5 @@ API_INSTANCE = "http://127.0.0.1:5000"
 username = "user"
 password = hash_password("123456")
 print(password)
-r = requests.get(f'{API_INSTANCE}/api/day/000000/{date.today().isoformat()}', headers={'X-User': username, 'X-Access-Hash': password})
+r = requests.get(f'{API_INSTANCE}/api/day/000000/{date.today().isoformat()}', headers={'X-User': username, 'X-Access-Hash': password}) # this is somewhat how it might look?
 print(r.text)
